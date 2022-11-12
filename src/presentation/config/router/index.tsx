b91ccbe49d31259dab2ec8995cdec34569798253
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-11-11 23:48:02 -0300

    feat: add app router

---
+++ src/presentation/config/router/index.tsx
@@ -0,0 +1,17 @@
+import '@/presentation/styles/global.scss'
+import React from 'react'
+import { Login } from '@/presentation/pages'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login"/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export { Router }
