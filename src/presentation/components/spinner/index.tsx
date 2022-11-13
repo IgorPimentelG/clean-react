26f6edefb1a5600a21fde98b1f0dc3929c634518
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-11-13 19:05:10 -0300

    feat: ensure Login present spinner on submit

--- src/presentation/components/spinner/index.tsx
+++ src/presentation/components/spinner/index.tsx
@@ -7,6 +7,7 @@ const Spinner: React.FC<Props> = (props: Props) => {
   return (
     <div
       {...props}
+      data-testId="spinner"
       className={[Styles.spinner, props.className].join(' ')}
     >
       <div />
