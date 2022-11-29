9a61164a370daf0c436232ac177994c9f0f74d4c
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-11-29 16:34:17 -0300

    test: ensure EmailValdation checkvalid emails

---
+++ src/presentation/validation/validators/email/email-validation.spec.ts
@@ -0,0 +1,19 @@
+import { EmailValidation } from './email-validation'
+import { InvalidFieldError } from '@/presentation/validation/errors'
+import faker from 'faker'
+
+const makeSut = (): EmailValidation => new EmailValidation(faker.database.column())
+
+describe('EmailValidation', () => {
+  test('Should return error if email is valid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.random.words())
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+})
