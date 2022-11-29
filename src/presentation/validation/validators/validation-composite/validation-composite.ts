ad1b47390c4c7101cb7f2f79335ae05e4a3dd184
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-11-29 17:37:47 -0300

    feat: add validation composite

---
+++ src/presentation/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,18 @@
+import { FieldValidation } from '@/presentation/validation/protocols'
+import { Validation } from '@/presentation/protocols/validation'
+
+class ValidationComposite implements Validation {
+  constructor (private readonly validators: FieldValidation[]) {}
+
+  validate (fieldName: string, fieldValue: string): string {
+    const validators = this.validators.filter(v => v.field === fieldName)
+    for (const validator of validators) {
+      const error = validator.validate(fieldValue)
+      if (error) {
+        return error.message
+      }
+    }
+  }
+}
+
+export { ValidationComposite }
