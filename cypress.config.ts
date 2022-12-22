37c5dbc49acfdc7e70bc15409eb97c899de95b42
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-12-22 15:58:41 -0300

    test: ensure Login works

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
+    env: {
+      api: "http://localhost:5050/api"
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/authentication/login.spec.cy.ts
+++ cypress/e2e/authentication/login.spec.cy.ts
@@ -1,5 +1,79 @@
-describe("", () => {
+import faker from "faker"
+
+const baseUrl: string = Cypress.config().baseUrl;
+
+describe("Login", () => {
+    beforeEach(() => {
+        cy.visit("/login");
+    });
+
     it("Should load with correct initial state", () => {
-        
-    })
+        cy.getByTestId("email").should("have.attr", "readOnly");
+        cy.getByTestId("email-status")
+            .should("have.attr", "title", "Campo obrigatório")
+            .should("contain.text", "🔴");
+        cy.getByTestId("password").should("have.attr", "readOnly");
+        cy.getByTestId("password-status")
+            .should("have.attr", "title", "Campo obrigatório")
+            .should("contain.text", "🔴");;
+        cy.getByTestId("submit").should("have.attr", "disabled");
+        cy.getByTestId("error-wrap").should("not.have.descendants");
+    });
+
+    it("Should present error state if form is invalid", () => {
+        cy.getByTestId("email").focus().type(faker.random.word());
+        cy.getByTestId("email-status")
+            .should("have.attr", "title", "Valor Inválido")
+            .should("contain.text", "🔴");
+        cy.getByTestId("password").focus().type(faker.random.alphaNumeric(3));
+        cy.getByTestId("password-status")
+            .should("have.attr", "title", "Valor Inválido")
+            .should("contain.text", "🔴");
+        cy.getByTestId("submit").should("have.attr", "disabled");
+        cy.getByTestId("error-wrap").should("not.have.descendants");
+    });
+
+    it("Should present valid state if form is valid", () => {
+        cy.getByTestId("email").focus().type(faker.internet.email());
+        cy.getByTestId("email-status")
+            .should("have.attr", "title", "Tudo certo!")
+            .should("contain.text", "🟢");
+        cy.getByTestId("password").focus().type(faker.random.alphaNumeric(6));
+        cy.getByTestId("password-status")
+            .should("have.attr", "title", "Tudo certo!")
+            .should("contain.text", "🟢");
+        cy.getByTestId("submit").should("not.have.attr", "disabled");
+        cy.getByTestId("error-wrap").should("not.have.descendants");
+    });
+
+    it("Should present error if invalid credentials are provided", () => {
+        cy.getByTestId("email").focus().type(faker.internet.email());
+        cy.getByTestId("password").focus().type(faker.random.alphaNumeric(6));
+        cy.getByTestId("submit").click();
+        cy.getByTestId("error-wrap")
+            .getByTestId("spinner").should("exist")
+            .getByTestId("error").should("not.exist")
+            .getByTestId("spinner").should("not.exist")
+            .getByTestId("error").should("contain.text", "Credenciais inválidas");
+        cy.url().should('eq', `${baseUrl}/login`);
+    });
+
+    it("Should present save accessToken if valid credentails are provided", () => {
+        cy.intercept("POST", `${Cypress.env("api")}/login`, { 
+            statusCode: 200,
+            body: {
+                accessToken: faker.random.uuid()
+            }
+        });
+
+        cy.getByTestId("email").focus().type(faker.internet.email());
+        cy.getByTestId("password").focus().type(faker.random.alphaNumeric(6));
+        cy.getByTestId("submit").click();
+        cy.getByTestId("error-wrap")
+            .getByTestId("spinner").should("exist")
+            .getByTestId("error").should("not.exist")
+            .getByTestId("spinner").should("not.exist")
+        cy.url().should('eq', `${baseUrl}/login`);
+        cy.window().then(window => assert.isOk(window.localStorage.getItem("accessToken")));
+    });
 })
