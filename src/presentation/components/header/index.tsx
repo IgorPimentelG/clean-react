ed30f87daba48bc07a17d24cfd4beb3b638e5e07
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-12-23 17:57:53 -0300

    feat: add header and icon components

---
+++ src/presentation/components/header/index.tsx
@@ -0,0 +1,19 @@
+import React, { memo } from 'react'
+import { Logo } from '../logo'
+import styles from './styles.scss'
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.headerWrap}>
+      <div className={styles.headerContent}>
+        <Logo />
+        <div className={styles.logoutWrap}>
+          <span>Igor</span>
+          <a href="#">Sair</a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default memo(Header)
--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -1,27 +1,20 @@
-import { Logo, Footer } from '@/presentation/components'
+import { Header, Footer, Icon, IconName } from '@/presentation/components'
 import React from 'react'
 import styles from './styles.scss'
 
 const SurveyList: React.FC = () => {
   return (
     <div className={styles.surveyListWrap}>
-      <header className={styles.headerWrap}>
-        <div className={styles.headerContent}>
-          <Logo/>
-          <div className={styles.logoutWrap}>
-            <span>Igor</span>
-            <a href="#">Sair</a>
-          </div>
-        </div>
-      </header>
+      <Header />
       <div className={styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul>
           <li>
             <div className={styles.surveyContent}>
-              <div className={[styles.iconWrap, styles.red].join(' ')}>
-                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII="/>
-              </div>
+              <Icon
+                iconName={IconName.thumbDown}
+                className={styles.icon}
+              />
               <time>
                 <span className={styles.day}>25</span>
                 <span className={styles.month}>12</span>
