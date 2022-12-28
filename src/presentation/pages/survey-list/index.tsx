7d6461d16187812cfa766fecbb8df24d06ff8cc2
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-12-27 21:37:03 -0300

    feat: ensure SurveyList render SurveyItems on success

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -1,18 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.scss'
 import { Header, Footer } from '@/presentation/components'
 import { LoadSurveyList } from '@/domain/usecases'
-import { SurveyItemEmpty } from './components'
+import { SurveyItem, SurveyItemEmpty } from './components'
+import { SurveyModel } from '@/domain/models'
 
 type Props = {
   loadSurveyList: LoadSurveyList
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const [state, setState] = useState({
+    surveys: [] as SurveyModel[],
+    error: ''
+  })
+
   useEffect(() => {
-    (async function () {
-      loadSurveyList.loadAll()
-    })()
+    loadSurveyList.loadAll()
+      .then((surveys) => {
+        setState({ ...state, surveys })
+      }).catch((error) => {
+        setState({ ...state, error: error.message })
+      })
   }, [])
 
   return (
@@ -20,9 +29,21 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header />
       <div className={styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul data-testid="survey-list">
-          <SurveyItemEmpty />
-        </ul>
+        {state.error
+          ? (
+            <div>
+              <span data-testid="error">{state.error}</span>
+              <button>Recarregar</button>
+            </div>
+          ) : (
+            <ul data-testid="survey-list">
+              {state.surveys.length
+                ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey}/>)
+                : (<SurveyItemEmpty />)
+              }
+            </ul>
+          )
+        }
       </div>
       <Footer />
     </div>
