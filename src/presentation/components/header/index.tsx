3738452fda192ada37bb71ef1a77e412953ed061
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-12-29 12:02:08 -0300

    feat: add logout and show username

--- src/presentation/components/header/index.tsx
+++ src/presentation/components/header/index.tsx
@@ -1,15 +1,26 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
 import { Logo } from '../logo'
 import styles from './styles.module.scss'
+import { APIContext } from '@/presentation/context'
+import { useNavigate } from 'react-router-dom'
 
 const Header: React.FC = () => {
+  const { setCurrentAccount, getCurrentAccount } = useContext(APIContext)
+  const navigate = useNavigate()
+
+  function logout (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void {
+    event.preventDefault()
+    setCurrentAccount(undefined)
+    navigate('/login', { replace: true })
+  }
+
   return (
     <header className={styles.headerWrap}>
       <div className={styles.headerContent}>
         <Logo />
         <div className={styles.logoutWrap}>
-          <span>Igor</span>
-          <a href="#">Sair</a>
+          <span data-testid="username">{getCurrentAccount().name}</span>
+          <a data-testid="logout" href="#" onClick={logout}>Sair</a>
         </div>
       </div>
     </header>
