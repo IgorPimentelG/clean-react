2ad3ef72fdcb84a76232bc526c0c3a55884b1ed0
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-12-29 10:46:44 -0300

    feat: add authorize http decorator factory

--- src/main/factories/usecases/authentication/remote-add-account-factory.ts
+++ src/main/factories/usecases/authentication/remote-add-account-factory.ts
@@ -1,7 +1,6 @@
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
 import { AddAccount } from '@/domain/usecases'
-import { makeApiUrl } from '@/main/factories/http/api-url-factory'
-import { RemoteAddAccount } from '@/data/usecases/add-account/remote-add-account'
+import { RemoteAddAccount } from '@/data/usecases'
+import { makeAxiosHttpClient, makeApiUrl } from '@/main/factories/http'
 
 const makeRemoteAddAccount = (): AddAccount => {
   return new RemoteAddAccount(makeApiUrl('/signup'), makeAxiosHttpClient())
