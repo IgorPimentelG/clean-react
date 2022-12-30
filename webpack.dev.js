97b8709fe646c1710e1378f5aa94da0535b99fa4
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-12-30 10:50:26 -0300

    chore: add webpack prod and dev configs

---
+++ webpack.dev.js
@@ -0,0 +1,49 @@
+const { DefinePlugin } = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.(ts)x?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'ts-loader',
+        options: {
+          compilerOptions: { noEmit: false }
+        }
+      }
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
+  },
+  devServer: {
+    static: './public',
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('http://localhost:5050/api')
+    }),
+    new HTMLWebpackPlugin({
+      template: 'template.dev.html'
+    })
+  ]
+})
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,11 @@
-const path = require('path')
 const { DefinePlugin } = require('webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/main/index.tsx',
-  output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
-    alias: {
-      '@': path.join(__dirname, 'src')
-    }
-  },
+module.exports = merge(common, {
+  mode: 'production',
   module: {
     rules: [{
       test: /\.(ts)x?$/,
@@ -29,7 +19,7 @@ module.exports = {
     }, {
       test: /\.scss$/,
       use: [{
-        loader: 'style-loader'
+        loader: MiniCSSExtractPlugin.loader
       }, {
         loader: 'css-loader',
         options: {
@@ -40,21 +30,19 @@ module.exports = {
       }]
     }]
   },
-  devServer: {
-    static: './public',
-    historyApiFallback: true,
-    devMiddleware: {
-      writeToDisk: true
-    }
-  },
   externals: {
     react: 'React',
     'react-dom': 'ReactDOM'
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('http://localhost:5050/api')
+    }),
+    new HTMLWebpackPlugin({
+      template: 'template.prod.html'
+    }),
+    new MiniCSSExtractPlugin({
+      filename: 'main-bundle-[hash].css'
     })
   ]
-}
+})
